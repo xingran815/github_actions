963ada8a55cde911fbd2d5e2aeadd7b5b664f7d1
xingran815 <xingran_wang@icloud.com>
2025-11-05 12:04:00 +0000

    removing unused import and fixing spacing

--- file_test.py
+++ file_test.py
@@ -1,6 +1,3 @@
-import pytest
-
-
 def test_calc_addition():
     output = 2+4
     assert output == 6
@@ -20,5 +17,5 @@ def test_calc_multiply():
 
 def test_coucou():
     # Function test if the output return 'hello'
-    output='hello'
+    output = 'hello'
     assert output == 'hello'
